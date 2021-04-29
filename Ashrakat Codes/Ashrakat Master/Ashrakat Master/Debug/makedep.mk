a871ed26e31260f633c28982d9f6acae88c2154b
ashrakat1910 <75751982+ashrakat1910@users.noreply.github.com>
2021-04-29 16:37:43 +0200

    initial commit

---
+++ Ashrakat Codes/Ashrakat Master/Ashrakat Master/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\master_main.c
+
+MCAL\SPI\SPI.c
+
+MCAL\UART\USART.c
+
---
+++ Ashrakat Codes/Ashrakt Slave/Ashrakt Slave/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+APP\slave_main.c
+
+HAL\LCD\LCD.c
+
+MCAL\SPI\SPI.c
+
+MCAL\UART\USART.c
+
